338df6cf21e76d5c113d178e5b196c3eeb71d341
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-03-01 21:19:26 +0900

    first commit

---
+++ app/eneos/page.js
@@ -0,0 +1,25 @@
+function Eneos(){
+    return(
+        <>
+         <h1 className="my-12 text-2xl text-center flex justify-center">ENEOS ページ</h1>
+            <div className="flex flex-col items-start gap-12 mx-8">
+                <article>
+                    <h2>【アプリ】</h2>
+                    <div>
+                        <p>ログインID: happiness6.f21@icloud.com</p>
+                        <p>パスワード: Tomomi0720</p>
+                    </div>
+                </article>
+
+                <article>
+                    <h2>【VISAカード(ENEOSカード)】</h2>
+                    <div>
+                        <p>暗証番号: 4372</p>
+                        <p>セキュリティ番号: 456</p>
+                    </div>
+                </article>
+            </div>
+        </>
+    )
+}
+export default Eneos;
